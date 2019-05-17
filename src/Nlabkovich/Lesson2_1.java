a55f416a2a511b1a559aadfc9cb98552769fc680
NLabkovich <2967098@tut.by>
2019-05-17 21:34:05 +0300

    Work my stady

---
+++ src/Nlabkovich/Lesson2_1.java
@@ -0,0 +1,11 @@
+package Nlabkovich;
+
+public class Lesson2_1 {
+    public static void main(String[] args) {
+    System.out.println("Я ");
+    System.out.println("изучаю ");
+    System.out.println("ява ");
+    System.out.println("второй ");
+    System.out.println("день");
+}
+}
