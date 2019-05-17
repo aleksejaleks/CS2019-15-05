f8d9dd50729a39f86ef565b3b75e58684d1e9386
dmbelov <dmbelov777@gmail.com>
2019-05-17 22:55:19 +0300

    Done Lesson 2 full Task A

---
+++ src/belov/Lesson2TaskA1.java
@@ -0,0 +1,7 @@
+package belov;
+
+public class Lesson2TaskA1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/belov/Lesson2TaskA2.java
@@ -0,0 +1,12 @@
+package belov;
+
+public class Lesson2TaskA2 {
+    public static void main(String[] args) {
+        int i = 0;
+        String text = "Я начинаю изучать Java!";
+        while (i < 5) {
+            System.out.println(text);
+            i++;
+        }
+    }
+}
