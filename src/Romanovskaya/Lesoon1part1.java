65f189a0e2e8b8959a7d3599ab1a922869ad0779
MaryamaYoongi <synkhakhon@gmail.com>
2019-05-17 21:17:37 +0300

    Lesson 1 - 3 задания

---
+++ src/Romanovskaya/Lesoon1part1.java
@@ -0,0 +1,11 @@
+package Romanovskaya;
+
+public class Lesoon1part1
+    {
+        public static void main(String [] args)
+        {
+            System.out.println ("Hello world!");
+        }
+
+
+}
