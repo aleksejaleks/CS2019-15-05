d64fc180f8eeaa8f2e16448841b2bbfa2e612458
eyakunin <egor.yakunin@gmail.com>
2019-05-17 21:15:22 +0300

    ok

---
+++ src/eyakunin/task2.java
@@ -0,0 +1,12 @@
+package eyakunin;
+
+public class task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Я начинаю изучать JAVA!");
+        System.out.println("Я начинаю изучать JAVA!");
+        System.out.println("Я начинаю изучать JAVA!");
+        System.out.println("Я начинаю изучать JAVA!");
+        System.out.println("Я начинаю изучать JAVA!");
+    }
+}
