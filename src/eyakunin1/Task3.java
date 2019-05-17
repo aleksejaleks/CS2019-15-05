050e028f8878ef26e0e0cebfcbc356fc32bdae3d
eyakunin <egor.yakunin@gmail.com>
2019-05-17 21:35:45 +0300

    completed task 3

---
+++ src/eyakunin1/Task3.java
@@ -0,0 +1,12 @@
+package eyakunin1;
+
+public class Task3 {
+    public static void main (String [] args){
+        int i = 3;
+        int j = 4;
+        int ii = i * i;
+        int jj = j * j;
+        int k = ii + jj;
+        System.out.println(i + " * " + i + " + " + j + " * " + j + " = " + k);
+    }
+}
