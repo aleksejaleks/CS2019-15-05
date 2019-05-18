773bb7e36227d1863a80118e57307c6af3d6c00c
Maksimvoznyuk <voznukmax@gmail.com>
2019-05-19 00:56:16 +0300

    Done: C2 task + C1 task update

--- src/maksimvoznyuk/Lesson2TaskC1.java
+++ src/maksimvoznyuk/Lesson2TaskC1.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Lesson2TaskC1 {
     public static void main (String args []){
         System.out.println ("Введите число 1: ");
-        Scanner scan1 = new Scanner(System.in);
-        int a = scan1.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
 
         System.out.println ("Введите число 2: ");
-        Scanner scan2 = new Scanner(System.in);
-        int b = scan2.nextInt();
+        int b = scan.nextInt();
 
         int c = a + b;
-        System.out.println ("Сумма равна" + c);
+        System.out.println ("Сумма равна " + c);
     }
 }
--- src/maksimvoznyuk/Lesson2TaskC2.java
+++ src/maksimvoznyuk/Lesson2TaskC2.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Lesson2TaskC2 {
     public static void main (String args []){
         System.out.println ("Введите число 1: ");
-        Scanner scan1 = new Scanner(System.in);
-        int a = scan1.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
 
         System.out.println ("Введите число 2: ");
-        Scanner scan2 = new Scanner(System.in);
-        int b = scan2.nextInt();
+        int b = scan.nextInt();
 
         int c = a + b;
         System.out.println ("DEC: " + a + "+" + b + "=" + c);
