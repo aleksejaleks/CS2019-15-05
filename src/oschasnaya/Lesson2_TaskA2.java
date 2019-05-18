ec5c53224b6acadada2e4245df20d402c2da8b74
Ольга <schasnaya91@gmail.com>
2019-05-18 15:06:47 +0300

    L2 A2+A3

---
+++ src/oschasnaya/Lesson2_TaskA2.java
@@ -0,0 +1,12 @@
+package oschasnaya;
+
+public class Lesson2_TaskA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+}
+}
