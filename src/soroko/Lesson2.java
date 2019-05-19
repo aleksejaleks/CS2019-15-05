a4c6e0daff612050efb411d3ddb309ae0b46826a
User <Gingerbread.xxkpu1@tut.by>
2019-05-19 23:49:00 +0300

    Задачи второе занятие

---
+++ src/soroko/Lesson2.java
@@ -0,0 +1,13 @@
+package soroko;
+
+public class Lesson2 {
+
+    public static void main(String[] args){
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        }
+    }
+
---
+++ src/soroko/Lesson2Task1.java
@@ -0,0 +1,8 @@
+package soroko;
+
+public class Lesson2Task1 {
+
+    public static void main(String[] args) {System.out.println("Hello World!");
+        }
+    }
+
