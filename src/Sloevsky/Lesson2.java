e65eb5b9aa4503471a90b119a02e8e2a445d6124
Администратор <k.sloevsky@gmail.com>
2019-05-19 12:36:23 +0300

    lesson2

---
+++ src/Sloevsky/Lesson2.java
@@ -0,0 +1,11 @@
+package Sloevsky;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 4;
+        int k = i * i + j * j;
+        System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
+    }
+
+}
